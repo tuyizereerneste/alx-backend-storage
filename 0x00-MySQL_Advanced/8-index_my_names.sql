6a4175add30a962277c9cdcbc484d00122f6f12e
tuyizereerneste <tuyizereernest@yahoo.com>
2024-03-21 18:07:03 +0200

    index created

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,4 @@
 -- script that creates an index idx_name_first on
 -- the table names and the first letter of name.
+-- Only the first letter of name must be indexed
 CREATE INDEX idx_first_name ON names(name(1))
