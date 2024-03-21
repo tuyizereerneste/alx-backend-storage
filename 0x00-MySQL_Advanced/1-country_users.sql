2e5b237b36ad711dc5c32b3a472ab85d84e6f13b
tuyizereerneste <tuyizereernest@yahoo.com>
2024-03-21 13:57:16 +0200

    ENUM

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS `users` (
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `email` VARCHAR(255) NOT NULL UNIQUE,
     `name` VARCHAR(255),
-    `country` CHAR(2) CHECK (country IN ("US", "CO", "TN")) NOT NULL DEFAULT "US"
+    `country` ENUM("US", "CO", "TN") NOT NULL DEFAULT "US"
 );
